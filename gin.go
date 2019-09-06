216d7b3dcb3940245baef9702ca89acdc89fe0aa
xiongjianbo <10044375@qq.com>
2019-09-06 17:36:14 +0800

    s

--- gin.go
+++ gin.go
@@ -4,9 +4,10 @@ import (
 	"flag"
 	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
 )
 
-func Start(address string, debug string, route func(router *gin.Engine)) {
+func Start(address string, debug string, route func(router *gin.Engine) gin.IRoutes) {
 	isDaemon := flag.Bool("d", false, "守护进程模式运行程序,例:yunlifang-dial -d");
 	stopDaemon := flag.Bool("stop", false, "退出守护模式程序,例:yunlifang-dial -stop");
 	flag.Parse()
@@ -32,7 +33,26 @@ func Start(address string, debug string, route func(router *gin.Engine)) {
 	}
 
 	app := gin.Default()
-	route(app)
+	router := route(app)
+	router.Use(Cors())
 
 	app.Run(address)
+}
+
+/*中间件 默认开启*/
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		/*放行所有OPTIONS方法*/
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+		/*处理请求*/
+		c.Next()
+	}
 }
