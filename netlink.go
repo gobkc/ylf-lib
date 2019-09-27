38d8972cbe595c63885e830f1f4b3822dccb01f0
xiongjianbo <10044375@qq.com>
2019-09-27 11:55:52 +0800

    fix

--- netlink.go
+++ netlink.go
@@ -156,7 +156,7 @@ func SetDeviceIP(name string, ip string) error {
 
 /*添加macVLan 并给macVLan添加MAC地址*/
 func AddMacVLan(macVLanName string, eth string, macAddress string) error {
-	cmdString := fmt.Sprintf("ip link add link %s dev %s address %s type macvlan && ip link set %s up", eth, macVLanName, macAddress, macVLanName)
+	cmdString := fmt.Sprintf("ip link del link %s dev %s && ip link add link %s dev %s address %s type macvlan && ip link set %s up", eth, macVLanName, eth, macVLanName, macAddress, macVLanName)
 	cmd := exec.Command("bash", "-c", cmdString)
 	if _, err := cmd.Output(); err != nil {
 		log.Println(errors.New("添加macVLan失败,详细原因:" + err.Error()))
