6eaadebab040b814fb37697e3655e10a788ec9d8
xiongjianbo <10044375@qq.com>
2019-10-08 15:38:11 +0800

    s

--- kill.go
+++ kill.go
@@ -2,7 +2,11 @@ package ylf
 
 import (
 	"errors"
+	"fmt"
+	"os"
 	"os/exec"
+	"os/signal"
+	"sync"
 )
 
 func KillPid(pid string) error {
@@ -11,4 +15,22 @@ func KillPid(pid string) error {
 		return errors.New(pid + "进程不存在或已经被杀掉了")
 	}
 	return nil
+}
+
+
+func CtrlC(fc func(signalTag string)) {
+	var wg sync.WaitGroup
+	/*waitGroup内部计数器+1,为0都时wait阻塞会释放，必须在goroutine执行之前执行你该*/
+	wg.Add(1)
+	/*将键盘输入信号转发到osSignal*/
+	osSignal := make(chan os.Signal)
+	signal.Notify(osSignal)
+	/*goroutine开始*/
+	/*goroutine结束*/
+	select {
+	case sig := <-osSignal:
+		fc(fmt.Sprintf("%s", sig))
+		os.Exit(1)
+	}
+	wg.Wait()
 }
